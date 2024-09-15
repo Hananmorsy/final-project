c5e40313f81910bfeb9a9227b3e70200dfd3d135
Hananmorsy <hananmorsy1980@gmail.com>
2024-09-16 01:27:31 +0300

    final Project

--- src/component/Button/Button.jsx
+++ src/component/Button/Button.jsx
@@ -3,17 +3,18 @@ import './Button.css'
 function Button() {
 
     return <div className='btn'>
-        <a href="http://localhost:5182/Home">
+        <a href="http://localhost:5173/Home">
         <button className='btn1'>Home</button>
+        
         </a>
-        <a href="http://localhost:5182/Shop">
+        <a href="http://localhost:5173/Shop">
         <button className='btn1' >Shop</button>
         </a>
 
-        <a href="http://localhost:5182/Product/1">
+        <a href="http://localhost:5173/Cart">
         <button className='btn1'>About</button>
         </a>
-        <a href="http://localhost:5182/contact">
+        <a href="http://localhost:5173/contact">
         <button className='btn1'>Contact</button>
         </a>
                
